8278ff1b8b34ebf29cee83bf4c044f883b0e8905
Nate Ferbrache <nateferb@outlook.com>
2022-12-15 11:05:43 +0000

    Create experiments.cpp

---
+++ experiments.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+char ycoord, xcoord, position[0][0][0];
+
+void oTurn() {
+  std::cout << "Player o's Turn!\n";
+  std::cout << "Enter y coordinate (a, b or c): ";
+  std::cin >> ycoord;
+  std::cout << "Enter x coordinate (1, 2 or 3): ";
+  std::cin >> xcoord;
+  for (int i = 0; i <= 5;) {
+		
+	}
+}
