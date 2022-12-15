5bc0d85ac4d4ed297864ac441409d45668f3da4e
Nate Ferbrache <nateferb@outlook.com>
2022-12-15 11:05:13 +0000

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,64 +1,51 @@
-#include <iostream>
-#include <random>
+#include "functions.h"
 
-char position[3][3][3];
+int solo() {
+  std::cout << "Do you want to be noughts (o) or crosses (x)? ";
+  std::cin >> xoro;
 
-void displayGrid() {
-	std::cout << "\t1\t2\t3\n";
-	std::cout << "a\t" << position[0][0][0] << "\t" << position[0][1][0] << "\t" << position[0][2][0] << "\n";
-	std::cout << "b\t" << position[1][0][1] << "\t" << position[1][1][1] << "\t" << position[1][2][1] << "\n";
-	std::cout << "c\t" << position[2][0][2] << "\t" << position[2][1][2] << "\t" << position[2][2][2] << "\n\n";
+  if (xoro == 'o') {
+    std::cout << "\nNoughts (o) Selected!\n\n";
+  } else if (xoro == 'x') {
+    std::cout << "\nCrosses (x) Selected\n\n";
+  } else {
+    std::cout << "please select either 'x' or 'o'.\n";
+    solo();
+  }
+  return 0;
+}
+
+int multiplayer() {
+  std::cout << "Player x, What is your name? ";
+  std::cin >> p1name;
+  std::cout << "Player o, What is your name? ";
+  std::cin >> p2name;
+
+  while (winner != 'x' || 'o') {
+    displayGrid();
+    xTurn();
+    displayGrid();
+    oTurn();
+  }
+
+  std::cout << "\n\nDo you want to play again?\nType 'y' or 'n': ";
+  std::cin >> playAgain;
+  if (playAgain == 'y') {
+    multiplayer();
+  } else if (playAgain == 'n') {
+    std::cout << "OK, bye then!";
+  }
 }
 
 int main() {
-	char norc;
-	char playAgain;
-	char xcoord;
-	char ycoord;
-
-	std::cout << "INSTRUCTIONS:\n";
-	std::cout << "Choose either 'x' or 'o' to begin. Type coordinates in the format '2a'\n\n";
-
-	std::cout << "Do you want to be noughts (o) or crosses (x)? ";
-	std::cin >> norc;
-
-	if (norc == 'o') {
-		std::cout << "\nNoughts (o) Selected!\n\n";
-	}
-	else if (norc == 'x') {
-		std::cout << "\nCrosses (x) Selected\n\n";
-	}
-	else {
-		// Select either 'x' or 'o' randomly for user
-		// std::cout << "Since you are not competant enough to choose an option, we have chosen " << norc << "\n";
-	}
-
-	displayGrid();
-
-	std::cout << "Your Turn!\n";
-	std::cout << "Enter y coordinate (a, b or c): ";
-	std::cin >> ycoord;
-	std::cout << "Enter x coordinate (1, 2 or 3): ";
-	std::cin >> xcoord;
-	if (ycoord == 'a') {
-		if (xcoord == '1') {
-			position[0][0][0] = norc;
-		}
-		else if (xcoord == '2') {
-			
-		}
-		else if (xcoord == '3') {
-			
-		}
-	}
-	displayGrid();
-	
-	std::cout << "\n\nDo you want to play again?\nType 'y' or 'n': ";
-	std::cin >> playAgain;
-	if (playAgain == 'y') {
-		main();
-	}
-	else if (playAgain == 'n') {
-		std::cout << "OK, bye then!";
-	}
+  std::cout << "Multiplayer (m) or Single player (s)? ";
+  std::cin >> mors;
+  if (mors == 'm') {
+    multiplayer();
+  } else if (mors == 's') {
+    solo();
+  } else {
+    std::cout << "Please enter either 'm' or 's' to select the game mode.\n";
+    main();
+  }
 }
